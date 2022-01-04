d185b0937abd38f31e98c946281f339c94451ade
Oleg <52497929+Cortuzz@users.noreply.github.com>
2022-01-04 21:09:32 +0700

    Update reactor_control.lua

--- reactor_control.lua
+++ reactor_control.lua
@@ -10,7 +10,7 @@ local TEXT_COLOR = 0xFFD700
 local MAX_HEAT = reactor.getMaxHeat()
 
 function DrawChunk(chunk_number, value, max_value)
-  local CHUNK_SIZE = 10
+  CHUNK_SIZE = 10
   ratio = value / max_value
 
   gpu.set(27, 4, value / 1.6 .. "T / " .. max_value / 1.6 .. "T [" .. 100 * ratio .. "%]")  
