7e257757d49df5b43f63ad6796a2f58687fa352d
Oleg <52497929+Cortuzz@users.noreply.github.com>
2022-01-04 21:13:50 +0700

    units fix

--- reactor_control.lua
+++ reactor_control.lua
@@ -9,11 +9,11 @@ local BG_COLOR = 0x4B0082
 local TEXT_COLOR = 0xFFD700
 local MAX_HEAT = reactor.getMaxHeat()
 
-function DrawChunk(chunk_number, value, max_value)
+function DrawChunk(chunk_number, value, max_value, units)
   CHUNK_SIZE = 10
   ratio = value / max_value
 
-  gpu.set(27, 4, value / 1.6 .. "T / " .. max_value / 1.6 .. "T [" .. 100 * ratio .. "%]")  
+  gpu.set(27, 4, value .. units .. " / " .. max_value .. units .. " [" .. 100 * ratio .. "%]")  
   
   gpu.setBackground(0x888888)
   gpu.fill(5, 5 + 2 * chunk_number * CHUNK_SIZE, WIDTH - 10, CHUNK_SIZE, " ")
@@ -35,6 +35,6 @@ while true do
   current_heat = reactor.getHeat()
   current_chunk = 0
   
-  DrawChunk(current_chunk, current_heat, MAX_HEAT)
+  DrawChunk(current_chunk, current_heat / 1.2, MAX_HEAT / 1.2, "T")
   gpu.setBackground(BG_COLOR)
 end
