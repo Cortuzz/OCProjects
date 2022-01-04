29c84d09d1081566b6eda7844a343c16fe0d8de0
Oleg <52497929+Cortuzz@users.noreply.github.com>
2022-01-05 01:08:39 +0700

    Update reactor_control.lua

--- reactor_control.lua
+++ reactor_control.lua
@@ -1,9 +1,13 @@
 local term = require("term")
 local component = require("component")
 local reactor = component.reactor_chamber
+local note = require("note")
 local gpu = component.gpu
 
 local WIDTH, HEIGHT = gpu.getResolution()
+
+local WARNING_LEVEL = 0.75
+local CRITICAL_LEVEL = 0.95
 
 local CHUNK_SIZE = 8
 local BG_COLOR = 0x4B0082
@@ -31,8 +35,8 @@ function drawScale(chunk_number, first_threshold, second_threshold)
   local first_threshold_position = first_threshold * WIDTH - 10
   local second_threshold_position = second_threshold * WIDTH - 10
 
-  text(first_threshold_position + 1, chunk_height + 1, "OVERHEAT", 0xFF8C00)
-  text(second_threshold_position + 1, chunk_height + 1, "AUTO OFF", 0x8B0000)
+  write(first_threshold_position + 1, chunk_height + 1, "OVERHEAT", 0xFF8C00)
+  write(second_threshold_position + 1, chunk_height + 1, "AUTO OFF", 0x8B0000)
 
   gpu.setBackground(0x8B0000)
   gpu.fill(5, chunk_height, WIDTH - 10, 1, " ")
@@ -46,7 +50,7 @@ function drawScale(chunk_number, first_threshold, second_threshold)
   gpu.setBackground(BG_COLOR)
 end
 
-function text(first_position, second_position, text, color)
+function write(first_position, second_position, text, color)
   gpu.setForeground(color)
   gpu.set(first_position, second_position, text)
 
@@ -63,10 +67,19 @@ gpu.fill(1, 1, WIDTH, HEIGHT, " ")
 gpu.set(6, 4, "REACTOR TEMPERATURE:")
 
 while true do
-  local current_heat = reactor.getHeat()
   local current_chunk = 0
-  local current_heat = 6500
+  local current_heat = reactor.getHeat()
+  local heat_ratio = current_heat / MAX_HEAT
+
+  if heat_ratio > WARNING_LEVEL and heat_ratio < CRITICAL_LEVEL then
+    note.play(83, 1.05 - heat_ratio)
+  end
+  
+  if heat_ratio > CRITICAL_LEVEL then
+    note.play(83, 0.2)
+    note.play(88, 0.25)
+  end
   
   drawChunk(current_chunk, current_heat / 1.6, MAX_HEAT / 1.6, "T")
-  drawScale(current_chunk, 0.75, 0.95)
+  drawScale(current_chunk, WARNING_LEVEL, CRITICAL_LEVEL)
 end
