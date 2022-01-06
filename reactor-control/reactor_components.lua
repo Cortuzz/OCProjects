39a1a371af9f59e47daa1af1c27fdf54397e1928
Oleg <52497929+Cortuzz@users.noreply.github.com>
2022-01-07 01:10:56 +0700

    Update reactor_components.lua

--- reactor-control/reactor_components.lua
+++ reactor-control/reactor_components.lua
@@ -9,18 +9,30 @@ local CHUNK_SIZE = 5
 local FACTOR = CHUNK_SIZE + 1
 
 local BG_COLOR = 0x483D8B
-local TEXT_COLOR = 0xFFD700
 
 local REACTOR_WIDTH = 9
 local REACTOR_HEIGHT = 6
 
+--3, 7 for chunksize 5
 local DELTA_Y = 3
-local DELTA_X = 17
+local DELTA_X = 7
 
+local FUEL_COLOR = 0x4B0082
 
-function drawChunk(x_coord, y_coord, color)
-  gpu.setBackground(color)
-  gpu.fill(x_coord, y_coord, 2 * CHUNK_SIZE, CHUNK_SIZE, " ")
+function drawChunk(x_coord, y_coord, color, is_fuel)
+  if is_fuel then
+    gpu.setBackground(FUEL_COLOR)
+    gpu.fill(x_coord, y_coord, 2 * CHUNK_SIZE, CHUNK_SIZE, " ")
+
+    gpu.setBackground(color)
+    gpu.fill(x_coord + 2, y_coord + 1, 2 * CHUNK_SIZE - 4, CHUNK_SIZE - 2, " ")    
+
+    gpu.setBackground(FUEL_COLOR)
+    gpu.fill(x_coord + 4, y_coord + 2, 2 * CHUNK_SIZE - 8, CHUNK_SIZE - 4, " ")
+  else
+    gpu.setBackground(color)
+    gpu.fill(x_coord, y_coord, 2 * CHUNK_SIZE, CHUNK_SIZE, " ")
+  end
 
   gpu.setBackground(BG_COLOR)
 end
@@ -33,10 +45,11 @@ function materialsColoring(materials)
     if name == "minecraft:air" then
       materials[index].color = 0xC0C0C0
 
-    elseif string.find(name, "fuel") then
-      materials[index].color = 0x4B0082
-
     else
+      if string.find(name, "fuel") then
+        materials[index].fuel = true
+      end
+
       local damage_ratio = material.damage / material.maxDamage
     
       if damage_ratio < 0.2 then
@@ -67,7 +80,6 @@ end
 --------
 
 gpu.setBackground(BG_COLOR)
-gpu.setForeground(TEXT_COLOR)
 
 gpu.fill(1, 1, WIDTH, HEIGHT, " ")
 
@@ -79,8 +91,10 @@ while true do
     for j=1, REACTOR_HEIGHT do
       local pos_x = DELTA_X + 2 * FACTOR * i
       local pos_y = DELTA_Y + FACTOR * j
+      
+      local current_chunk = data[i + (j - 1) * REACTOR_WIDTH]
 
-      drawChunk(pos_x, pos_y, data[i + (j - 1) * REACTOR_WIDTH].color)
+      drawChunk(pos_x, pos_y, current_chunk.color, current_chunk.fuel)
     end
   end
 end
